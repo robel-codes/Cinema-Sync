701716b997410f34c2b1ab961c8dd2f677568563
Robel Gebremeskel <rofikre@yahoo.com>
2023-09-17 00:07:25 -0400

    Tab Navigator component added

--- src/api/apicalls.ts
+++ src/api/apicalls.ts
@@ -1,7 +1,8 @@
-import {apikey} from 'react-native-dotenv';
+import {apikey} from '@env';
 export const baseImagePath = (size: string, path: string) => {
   return `https://image.tmdb.org/t/p/${size}${path}`;
 };
+console.log(nowPlayingMovies);
 export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_playing?api_key${apikey}`;
 export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key${apikey}`;
 export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key${apikey}`;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {COLORS} from '../theme/theme';
+import {TouchableOpacity} from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}: any) => {
   return (
     <View style={styles.container}>
-      <Text>HomeScreen </Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.push('MovieDetails');
+        }}>
+        <Text>HomeScreen </Text>
+      </TouchableOpacity>
     </View>
   );
 };
